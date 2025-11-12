530c2f167f1dbf381f0b731ee5a5203f8385da92
DoYun-0 <dy060918@gmail.com>
2025-11-12 20:06:06 +0900

    fix: import name 수정

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import PokemonCard from "./pokemonCard";
+import PokemonCard from "./PokemonCard";
 import pokeball from "../assets/pokeball.png";
 
 const Dashboard = ({ selectPokemon, setSelectPokemon }) => {
